217c993ff76f8e56a70214912deb7d3f9d567f17
sensei-huang <80719864+sensei-huang@users.noreply.github.com>
2025-05-30 11:00:58 +1200

    Update PN532.ino

--- PN532.ino
+++ PN532.ino
@@ -1,3 +1,6 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_PN532.h>
@@ -7,10 +10,39 @@
 
 Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
 
-void setup(void) {
+ESP8266WebServer WebServer(80);
+
+uint8_t last_uid[] = { 0, 0, 0, 0, 0, 0, 0 };
+
+void defaultPage() {
+  char buffer[40];
+  sprintf(buffer, "%02x:%02x:%02x:%02x", last_uid[0], last_uid[1], last_uid[2], last_uid[3]);
+  WebServer.send(200, "text/plain", buffer);
+}
+
+void setup() {
   Serial.begin(115200);
-  delay(500);
-  Serial.println("Hello!");
+
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.softAPConfig(IPAddress(192, 168, 1, 1), IPAddress(192, 168, 1, 1), IPAddress(255, 255, 255, 0));
+  WiFi.softAP("iPad", "password");
+//  WiFi.mode(WIFI_STA);
+//  WiFi.begin("LPHS_BYOD", "kapukataumahaka"); 
+//  while(WiFi.status() != WL_CONNECTED) {
+//    Serial.print('.');
+//    delay(500);
+//  }
+  
+  if (MDNS.begin("info")){
+    Serial.println("mDNS responder started");
+  } else {
+    Serial.println("Error setting up MDNS responder!");
+  }
+  
+  WebServer.onNotFound(defaultPage);
+  WebServer.begin();
+  
+  MDNS.addService("http", "tcp", 80);
 
   nfc.begin();
 
@@ -33,25 +65,16 @@ void setup(void) {
   Serial.println("Waiting for an ISO14443A Card ...");
 }
 
-
-void loop(void) {
+void loop() {
   uint8_t success;
   uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
   uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
 
-  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
-  // 'uid' will be populated with the UID, and uidLength will indicate
-  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
   success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength, 1000);
 
   if (success) {
-    // Display some basic information about the card
-    Serial.println("Found an ISO14443A card");
-    Serial.print("  UID Length: ");
-    Serial.print(uidLength, DEC);
-    Serial.println(" bytes");
-    Serial.print("  UID Value: ");
-    nfc.PrintHex(uid, uidLength);
-    Serial.println("");
+    memcpy(last_uid, uid, sizeof(uid[0])*7);
   }
+  MDNS.update();
+  WebServer.handleClient();
 }
