a76ebfe820e4d62be8958da2e13876ed3ee6cca2
sensei-huang <80719864+sensei-huang@users.noreply.github.com>
2025-06-20 17:58:25 +1200

    Update and rename cardReader.ino to reader.ino

---
+++ reader.ino
@@ -0,0 +1,74 @@
+// Use 160MHz cpu
+
+//Software Serial library
+#include <SoftwareSerial.h>
+
+//PN532 libraries
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_PN532.h>
+
+//Software Serial defines
+#define rxPin D6
+#define txPin D7
+SoftwareSerial mySerial =  SoftwareSerial(rxPin, txPin);
+
+//PN532 defines+functions
+#define MSCHECK 1000
+#define MSREAD 50
+#define PN532_IRQ (D3)
+#define PN532_RESET (D4)
+Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
+uint8_t last_uid[] = { 0, 0, 0, 0, 0, 0, 0 };
+unsigned long last_detect = millis();
+void readCard(){
+  bool success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+  String read;
+  if(success){
+    memcpy(last_uid, uid, sizeof(uid[0])*7);
+    Serial.println("Card detected!");
+    last_detect = millis();
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  
+  //Software Serial init
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
+  mySerial.begin(9600);
+
+  //PN532 init
+  nfc.begin();
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (!versiondata) {
+    Serial.print("Didn't find PN53x board");
+    while (1);
+  }
+  Serial.print("Found chip PN5");
+  Serial.println((versiondata >> 24) & 0xFF, HEX);
+  Serial.print("Firmware ver. ");
+  Serial.print((versiondata >> 16) & 0xFF, DEC);
+  Serial.print('.');
+  Serial.println((versiondata >> 8) & 0xFF, DEC);
+  Serial.println("Waiting for an ISO14443A Card ...");
+}
+
+unsigned long last_read = millis();
+
+void loop() {
+  readCard();
+  if(millis()-last_read >= MSREAD){
+    if(millis()-last_detect >= MSCHECK){
+      mySerial.println("none");
+    }else{
+      char buffer[40];
+      sprintf(buffer, "%02x:%02x:%02x:%02x", last_uid[0], last_uid[1], last_uid[2], last_uid[3]);
+      mySerial.println(buffer);
+    }
+  }
+}
