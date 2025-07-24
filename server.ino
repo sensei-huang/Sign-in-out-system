40793275a921b11eb1c74437e19c4dae050a8a66
sensei-huang <80719864+sensei-huang@users.noreply.github.com>
2025-07-24 12:12:52 +1200

    Update server.ino

--- server.ino
+++ server.ino
@@ -105,7 +105,7 @@ void setup() {
 
   //Wifi init
   WiFi.mode(WIFI_STA);
-  WiFi.begin("LPHS_BYOD", "kapukataumahaka"); 
+  WiFi.begin("LPHS_BYOD"); 
   while(WiFi.status() != WL_CONNECTED) {
     Serial.print('.');
     delay(500);
